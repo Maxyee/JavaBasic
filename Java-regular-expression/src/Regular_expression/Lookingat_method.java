337c36432e8a72eee582f215281cfb755d27ae05
Julhas <mdjulhashossainmohon@gmail.com>
2017-06-22 08:31:47 +0600

    learning the lookingat and matches method for the regular expression and understand the concept

---
+++ Java-regular-expression/src/Regular_expression/Lookingat_method.java
@@ -0,0 +1,23 @@
+package Regular_expression;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Lookingat_method {
+	private static final String REGEX = "foo";
+	private static final String INPUT = "fooooooooooooooooo";
+	private static Pattern pattern;
+	private static Matcher matcher;
+	
+	public static void main(String args[])
+	{
+		pattern = Pattern.compile(REGEX);
+		matcher = pattern.matcher(INPUT);
+		
+		System.out.println("Current REGEX is:" +REGEX);
+		System.out.println("Current INPUT is:" +INPUT);
+		
+		System.out.println("lookingAt(): "+matcher.lookingAt());
+		System.out.println("matcher(): "+matcher.matches());
+	}
+}
