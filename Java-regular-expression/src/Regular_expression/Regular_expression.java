c73708f8b4719f34121e7c7f7fdc80e7766360e9
Julhas <mdjulhashossainmohon@gmail.com>
2017-06-22 07:57:03 +0600

    Java Regular expression study using the pattern and matcher Regex Library

---
+++ Java-regular-expression/src/Regular_expression/Regular_expression.java
@@ -0,0 +1,29 @@
+package Regular_expression;
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regular_expression {
+	
+	public static void main(String args[])
+	{
+		String line = "This order was placed for QT3000! OK?";
+		String pattern = "(.*)(\\d+)(.*)";
+		
+		Pattern p = Pattern.compile(pattern);
+		Matcher m = p.matcher(line);
+		
+		if(m.find())
+		{
+			System.out.println("Found value: " + m.group(0) );
+	        System.out.println("Found value: " + m.group(1) );
+	        System.out.println("Found value: " + m.group(2) );	
+		}
+		else
+		{
+			System.out.println("NO MATCH");
+		}
+		
+	}
+}
